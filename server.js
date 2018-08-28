410063c442657a461552267473bba2ce5d5ec895
Corey Johnson <probablycorey@github.com>
2018-08-28 13:40:26 -0700

    static deployment

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ require('noodle-it')
 const app = express()
 const port = process.env.PORT || 3001
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.get('/styles.', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
