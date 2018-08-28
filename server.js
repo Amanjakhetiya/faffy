9621c8b24286c6dcdef9eb3cec6f67e7e2cfb6e6
Corey Johnson <probablycorey@github.com>
2018-08-28 13:36:17 -0700

    dirname

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const path = require('path')
 require('noodle-it')
 
 const app = express()
 const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
+  console.log(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
