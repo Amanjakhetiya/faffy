f0996fd1a016fcaf3b7b5adf2f0b05146ec3d8a1
Corey Johnson <probablycorey@github.com>
2018-08-28 13:32:28 -0700

    just text

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express()
 const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
-  res.sendFile('./index.html')
+  res.send('hello')
+  // res.sendFile('./index.html')
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
